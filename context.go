e0f66f74bde99c32a851ae38895d3e47cf7cda07
Robert Williamson <williamson.robert@gmail.com>
2014-01-01 18:47:05 -0600

    Adjusting spacing to breaks things up when adding properites and callbacks.

--- context.go
+++ context.go
@@ -7,10 +7,13 @@ import (
 type Context clw.Context
 
 func CreateContext(d []Device) (Context, error) {
+
 	devices := make([]clw.DeviceID, len(d))
 	for i := range d {
 		devices[i] = clw.DeviceID(d[i].ID)
 	}
+
 	result, err := clw.CreateContext(nil, devices)
+
 	return Context(result), err
 }
