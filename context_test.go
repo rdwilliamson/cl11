b2763c47566ba564059955789320c443a182734b
Robert Williamson <williamson.robert@gmail.com>
2014-01-18 16:10:01 -0600

    Removed create contexts since it is now in create command queue.

--- context_test.go
+++ context_test.go
@@ -6,25 +6,6 @@ import (
 	"testing"
 )
 
-func TestCreateContext(t *testing.T) {
-	platforms, err := GetPlatforms()
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := range platforms {
-		devices, err := platforms[i].GetDevices()
-		if err != nil {
-			t.Fatal(err)
-		}
-		for j := range devices {
-			_, err = CreateContext(nil, []*Device{devices[j]}, func(err string, data []byte) {})
-			if err != nil {
-				t.Error(err)
-			}
-		}
-	}
-}
-
 func ExampleContext() {
 	platforms, err := GetPlatforms()
 	if err != nil {
