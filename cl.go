7840e3f6f719f0287d6cdfe3f1731a8e26cbed1a
Robert Williamson <williamson.robert@gmail.com>
2014-01-18 15:48:21 -0600

    Added flush and finish.

--- cl.go
+++ cl.go
@@ -1,2 +1,14 @@
 // A wrapper package that attempts to map the OpenCL 1.1 C API to idiomatic Go.
 package cl11
+
+import (
+	clw "github.com/rdwilliamson/clw11"
+)
+
+func Flush(cq CommandQueue) error {
+	return clw.Flush(cq.ID)
+}
+
+func Finish(cq CommandQueue) error {
+	return clw.Finish(cq.ID)
+}
