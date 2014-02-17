16e08263e3c18f550bb531a383abf3eaadd410b1
Robert Williamson <williamson.robert@gmail.com>
2014-02-17 00:12:35 -0600

    Have a way to pass in anything for buffer creation and probably kernel arguments.

--- cl.go
+++ cl.go
@@ -1,2 +1,71 @@
 // A wrapper package that attempts to map the OpenCL 1.1 C API to idiomatic Go.
 package cl11
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+var (
+	int32Type = reflect.TypeOf(int32(0))
+	int32Size = int32Type.Size()
+)
+
+// Scratch must be (at least) 8 bytes of scratch space.
+func toBytes(x interface{}, scratch unsafe.Pointer) []byte {
+
+	pointer, size := toPointerAndSize(x, scratch)
+
+	var header reflect.SliceHeader
+	header.Data = uintptr(pointer)
+	header.Len = int(size)
+	header.Cap = int(size)
+
+	return *(*[]byte)(unsafe.Pointer(&header))
+}
+
+func toPointerAndSize(x interface{}, scratch unsafe.Pointer) (unsafe.Pointer, uintptr) {
+
+	value := reflect.ValueOf(x)
+	switch value.Kind() {
+
+	case reflect.Int, reflect.Int32:
+		newValue := reflect.NewAt(int32Type, scratch).Elem()
+		newValue.SetInt(value.Int())
+		return safePointerAndSize(newValue)
+
+	case reflect.Ptr:
+
+		for {
+			value = value.Elem()
+			if value.Kind() != reflect.Ptr {
+				break
+			}
+		}
+		return safePointerAndSize(value)
+
+	case reflect.Slice:
+		return safePointerAndSize(value)
+
+	}
+	panic("unsupported kind")
+}
+
+func safePointerAndSize(v reflect.Value) (unsafe.Pointer, uintptr) {
+
+	switch v.Kind() {
+
+	case reflect.Int:
+		return unsafe.Pointer(v.UnsafeAddr()), int32Size
+
+	case reflect.Int32:
+		return unsafe.Pointer(v.UnsafeAddr()), v.Type().Size()
+
+	case reflect.Slice:
+		pointer := unsafe.Pointer(&(*reflect.SliceHeader)(unsafe.Pointer(v.Pointer())).Data)
+		size := v.Type().Elem().Size() * uintptr(v.Len())
+		return pointer, size
+
+	}
+	panic("unsupported kind")
+}
---
+++ cl_test.go
@@ -0,0 +1,79 @@
+package cl11
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func sizeCheck(want, got int, t *testing.T) {
+	if want != got {
+		t.Error("size mismatch, wanted", want, "got", got)
+	}
+}
+
+func pointerCheck(want, got unsafe.Pointer, t *testing.T) {
+	if want != got {
+		t.Error("pointer mismatch, wanted", want, "got", got)
+	}
+}
+
+// Bool
+
+func TestIntToBytes(t *testing.T) {
+
+	var scratchSpace [8]byte
+	scratch := unsafe.Pointer(&scratchSpace[0])
+
+	var anInt int = 1
+	bytes := toBytes(anInt, scratch)
+
+	sizeCheck(4, len(bytes), t)
+	pointerCheck(scratch, unsafe.Pointer(&bytes[0]), t)
+	if anInt != *(*int)(scratch) {
+		t.Error("value mismatch, wanted", anInt, "got", *(*int)(scratch))
+	}
+}
+
+// Int8
+// Int16
+// Int32
+// Int64
+// Uint
+// Uint8
+// Uint16
+// Uint32
+// Uint64
+// Uintptr
+// Float32
+// Float64
+// Complex64
+// Complex128
+// Array
+// Interface
+
+func TestPtrToBytes(t *testing.T) {
+
+	var scratchSpace [8]byte
+	scratch := unsafe.Pointer(&scratchSpace[0])
+
+	var anInt int
+	bytes := toBytes(&anInt, scratch)
+
+	sizeCheck(4, len(bytes), t)
+	pointerCheck(unsafe.Pointer(&anInt), unsafe.Pointer(&bytes[0]), t)
+}
+
+func TestSliceToBytes(t *testing.T) {
+
+	var scratchSpace [8]byte
+	scratch := unsafe.Pointer(&scratchSpace[0])
+
+	slice := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	bytes := toBytes(slice, scratch)
+
+	sizeCheck(len(slice)*4, len(bytes), t)
+	pointerCheck(unsafe.Pointer(&slice[0]), unsafe.Pointer(&bytes[0]), t)
+}
+
+// String
+// Struct
