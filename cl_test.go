5719db3fbd9d70e8fffdba74d235e91873aef6f6
Robert Williamson <williamson.robert@gmail.com>
2014-03-28 12:40:04 -0700

    Fixed tests.

--- cl_test.go
+++ cl_test.go
@@ -5,8 +5,10 @@ import (
 	"unsafe"
 )
 
-func sizeCheck(want, got int, t *testing.T) {
-	if want != got {
+var scratchSpace = unsafe.Pointer(new([scratchSize]byte))
+
+func sizeCheck(want int, got uintptr, t *testing.T) {
+	if want != int(got) {
 		t.Error("size mismatch, wanted", want, "got", got)
 	}
 }
@@ -19,24 +21,58 @@ func pointerCheck(want, got unsafe.Pointer, t *testing.T) {
 
 // Bool
 
-func TestIntToBytes(t *testing.T) {
+func TestIntPointerAndSize(t *testing.T) {
 
 	var scratchSpace [8]byte
 	scratch := unsafe.Pointer(&scratchSpace[0])
 
 	var anInt int = 1
-	bytes := ToBytes(anInt, scratch)
 
-	sizeCheck(4, len(bytes), t)
-	pointerCheck(scratch, unsafe.Pointer(&bytes[0]), t)
-	if anInt != *(*int)(scratch) {
+	pointer, size := getPointerAndSize(anInt, scratch)
+
+	sizeCheck(4, size, t)
+	pointerCheck(scratch, pointer, t)
+	if anInt != *(*int)(pointer) {
+		t.Error("value mismatch, wanted", anInt, "got", *(*int)(scratch))
+	}
+
+	pointer, size = getPointerAndSize(&anInt, scratch)
+
+	sizeCheck(4, size, t)
+	// Expect scratch since int needs to be converted to int32.
+	pointerCheck(scratch, pointer, t)
+	if anInt != *(*int)(pointer) {
 		t.Error("value mismatch, wanted", anInt, "got", *(*int)(scratch))
 	}
 }
 
 // Int8
 // Int16
-// Int32
+
+func TestInt32PointerAndSize(t *testing.T) {
+
+	var scratchSpace [8]byte
+	scratch := unsafe.Pointer(&scratchSpace[0])
+
+	var anInt int32 = 1
+
+	pointer, size := getPointerAndSize(anInt, scratch)
+
+	sizeCheck(4, size, t)
+	pointerCheck(scratch, pointer, t)
+	if anInt != *(*int32)(pointer) {
+		t.Error("value mismatch, wanted", anInt, "got", *(*int)(scratch))
+	}
+
+	pointer, size = getPointerAndSize(&anInt, scratch)
+
+	sizeCheck(4, size, t)
+	pointerCheck(unsafe.Pointer(&anInt), pointer, t)
+	if anInt != *(*int32)(pointer) {
+		t.Error("value mismatch, wanted", anInt, "got", *(*int)(scratch))
+	}
+}
+
 // Int64
 // Uint
 // Uint8
@@ -50,30 +86,7 @@ func TestIntToBytes(t *testing.T) {
 // Complex128
 // Array
 // Interface
-
-func TestPtrToBytes(t *testing.T) {
-
-	var scratchSpace [8]byte
-	scratch := unsafe.Pointer(&scratchSpace[0])
-
-	var anInt int
-	bytes := ToBytes(&anInt, scratch)
-
-	sizeCheck(4, len(bytes), t)
-	pointerCheck(unsafe.Pointer(&anInt), unsafe.Pointer(&bytes[0]), t)
-}
-
-func TestSliceToBytes(t *testing.T) {
-
-	var scratchSpace [8]byte
-	scratch := unsafe.Pointer(&scratchSpace[0])
-
-	slice := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	bytes := ToBytes(slice, scratch)
-
-	sizeCheck(len(slice)*4, len(bytes), t)
-	pointerCheck(unsafe.Pointer(&slice[0]), unsafe.Pointer(&bytes[0]), t)
-}
-
+// Ptr
+// Slice
 // String
 // Struct
--- mappedbuffer_test.go
+++ mappedbuffer_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestBufferMapping(t *testing.T) {
 	values := []float32{1, 2, 3, 4}
-	mapped := MappedBuffer{unsafe.Pointer(&values[0]), len(values) * int(float32Size), nil}
+	mapped := MappedBuffer{unsafe.Pointer(&values[0]), int64(len(values)) * int64(float32Size), nil}
 	slice := mapped.Float32Slice()
 	if !reflect.DeepEqual(values, slice) {
 		t.Errorf("float buffer mismatch")
