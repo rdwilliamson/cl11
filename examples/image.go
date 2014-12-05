e76cdd67831ddb8a8d1259f97e3982ed0a1467e5
Robert Williamson <williamson.robert@gmail.com>
2014-12-04 21:08:20 -0600

    Simplifying working examples.

--- examples/image.go
+++ examples/image.go
@@ -7,9 +7,6 @@ import (
 	"image/png"
 	"os"
 	"path/filepath"
-	"runtime"
-	"text/tabwriter"
-	"time"
 
 	cl "github.com/rdwilliamson/cl11"
 )
@@ -20,7 +17,7 @@ func check(err error) {
 	}
 }
 
-var kernel = `
+var kernel = []byte(`
 __constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_FILTER_NEAREST;
 
 __kernel void toGray(__read_only image2d_t input, __write_only image2d_t output)
@@ -41,15 +38,14 @@ __kernel void toGray(__read_only image2d_t input, __write_only image2d_t output)
 		}
 	}
 }
-`
+`)
 
-func openImage(file string) (image.Image, error) {
+func readImage(file string) (image.Image, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-
 	img, _, err := image.Decode(f)
 	if err != nil {
 		return nil, err
@@ -57,150 +53,84 @@ func openImage(file string) (image.Image, error) {
 	return img, nil
 }
 
+func writeImage(file string, img image.Image) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	err = png.Encode(f, img)
+	if err != nil {
+		f.Close()
+		return err
+	}
+	return f.Close()
+}
+
 func main() {
 
+	// Read the input file.
 	if len(os.Args) < 2 {
 		fmt.Println("no input file")
 		os.Exit(1)
 	}
-	input, err := openImage(os.Args[1])
+	input, err := readImage(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	width, height := input.Bounds().Dx(), input.Bounds().Dy()
 	output := image.NewRGBA(image.Rect(0, 0, width, height))
-
-	var count int
 	base := filepath.Base(os.Args[1])
 	ext := filepath.Ext(os.Args[1])
 	base = base[:len(base)-len(ext)]
 
-	outText := tabwriter.NewWriter(os.Stdout, 0, 1, 1, ' ', 0)
-
-	platforms, err := cl.GetPlatforms()
+	// Run the kernel on every device of every platform.
+	var count int
+	allPlatforms, err := cl.GetPlatforms()
 	check(err)
-	for _, p := range platforms {
-		for _, d := range p.Devices {
+	for _, platform := range allPlatforms {
+		for _, device := range platform.Devices {
 			count++
 
-			contextCallback := func(err string, data []byte, userData interface{}) {
-				fmt.Println(userData.(*cl.Device).Name, err)
-			}
-			c, err := cl.CreateContext([]*cl.Device{d}, nil, contextCallback, d)
+			// Create the OpenCL context.
+			c, err := cl.CreateContext([]*cl.Device{device}, nil, nil, nil)
 			check(err)
 
-			progam, err := c.CreateProgramWithSource([]byte(kernel))
-			check(err)
-
-			var options string
-			if d.Type == cl.DeviceTypeCpu {
-				options = "-D INTEGER"
-			}
-			err = progam.Build([]*cl.Device{d}, options, nil, nil)
-			if err != nil {
-				buildStatus, err := progam.BuildStatus(d)
-				check(err)
-				if buildStatus == cl.BuildError {
-					fmt.Println(progam.BuildLog(d))
-					os.Exit(1)
-				}
-			}
-			check(err)
-
-			kernel, err := progam.CreateKernel("toGray")
-			check(err)
-
-			cq, err := c.CreateCommandQueue(d, cl.QueueProfilingEnable)
-			check(err)
-
+			// Create the device buffers.
 			format := cl.ImageFormat{cl.RGBA, cl.UnsignedInt8}
-
 			outData, err := c.CreateDeviceImage(cl.MemWriteOnly, format, width, height, 1)
 			check(err)
-
 			inData, err := c.CreateDeviceImage(cl.MemReadOnly, format, width, height, 1)
 			check(err)
 
+			// Create the kernel and set its arguments.
+			progam, err := c.CreateProgramWithSource(kernel)
+			check(err)
+			var options string
+			if device.Type == cl.DeviceTypeCpu {
+				options = "-D INTEGER"
+			}
+			err = progam.Build([]*cl.Device{device}, options, nil, nil)
+			check(err)
+			kernel, err := progam.CreateKernel("toGray")
+			check(err)
 			err = kernel.SetArguments(inData, outData)
 			check(err)
 
-			localWidth := kernel.WorkGroupInfo[0].PreferredWorkGroupSizeMultiple
-			if runtime.GOOS == "darwin" && d.Type == cl.DeviceTypeCpu {
-				localWidth = 128
-			}
-			localHeight := d.MaxWorkGroupSize / localWidth
-			if localHeight > d.MaxWorkItemSizes[1] {
-				localHeight = d.MaxWorkItemSizes[1]
-			}
-
-			globalWidth, remainder := width/localWidth, width%localWidth
-			if remainder > 0 {
-				globalWidth++
-			}
-			globalWidth *= localWidth
-			globalHeight, remainder := height/localHeight, height%localHeight
-			if remainder > 0 {
-				globalHeight++
-			}
-			globalHeight *= localHeight
-
-			for i := 0; i < 2; i++ {
-
-				var readEvent, kernelEvent, writeEvent cl.Event
-
-				start := time.Now()
-
-				err = cq.EnqueueWriteImageFromImage(inData, cl.NonBlocking, input, nil, &writeEvent)
-				check(err)
-
-				err = cq.EnqueueNDRangeKernel(kernel, nil, []int{globalWidth, globalHeight},
-					[]int{localWidth, localHeight}, []*cl.Event{&writeEvent}, &kernelEvent)
-				check(err)
-
-				err = cq.EnqueueReadImageToImage(outData, cl.NonBlocking, output, []*cl.Event{&kernelEvent}, &readEvent)
-				check(err)
-
-				err = readEvent.Wait()
-				check(err)
-
-				if i == 1 {
-
-					wall := time.Since(start)
-
-					err = writeEvent.GetProfilingInfo()
-					check(err)
-					err = kernelEvent.GetProfilingInfo()
-					check(err)
-					err = readEvent.GetProfilingInfo()
-					check(err)
-
-					size := float64(width*height*4) / 1024 / 1024 / 1024
-
-					fmt.Fprintf(outText,
-						"%s\ton\t%s\tWrite (%v, %.2fGB/s)\tIdle (%v)\tKernel (%v)\tIdle (%v)\tRead (%v, %.2fGB/s)\t"+
-							"CPU Wall (%v)\n",
-						d.Name, p.Name,
-						time.Duration(writeEvent.End-writeEvent.Start),
-						size/time.Duration(writeEvent.End-writeEvent.Start).Seconds(),
-						time.Duration(kernelEvent.Start-writeEvent.End),
-						time.Duration(kernelEvent.End-kernelEvent.Start),
-						time.Duration(readEvent.Start-kernelEvent.End),
-						time.Duration(readEvent.End-readEvent.Start),
-						size/time.Duration(readEvent.End-readEvent.Start).Seconds(),
-						wall)
-				}
-			}
-
-			outFile, err := os.Create(fmt.Sprintf("%s%d%s", base, count, ext))
+			// Create the command queue then use it to copy the image to the
+			// device, run the kernel, and copy the result back.
+			cq, err := c.CreateCommandQueue(device, 0)
 			check(err)
-			defer outFile.Close()
+			err = cq.EnqueueWriteImageFromImage(inData, cl.NonBlocking, input, nil, nil)
+			check(err)
+			err = cq.EnqueueNDRangeKernel(kernel, nil, []int{width, height}, []int{128, 1}, nil, nil)
+			check(err)
+			err = cq.EnqueueReadImageToImage(outData, cl.Blocking, output, nil, nil)
+			check(err)
 
-			err = png.Encode(outFile, output)
+			// Write the result to a file.
+			err = writeImage(fmt.Sprintf("%s%d%s", base, count, ext), output)
 			check(err)
 		}
 	}
-
-	err = outText.Flush()
-	check(err)
 }
