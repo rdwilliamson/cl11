992cb11ff13935e55dff9808f9ba710c369faea7
Robert Williamson <williamson.robert@gmail.com>
2014-12-20 10:41:45 -0600

    Added mapped buffer read test.

--- mappedbuffer_test.go
+++ mappedbuffer_test.go
@@ -1,6 +1,7 @@
 package cl11
 
 import (
+	"bytes"
 	"io"
 	"testing"
 )
@@ -65,6 +66,63 @@ func TestMappedBufferRead(t *testing.T) {
 		n, err = map0.Read(everything)
 		if n != 0 || err != io.EOF {
 			t.Error("reading past end of buffer: want 0 io.EOF, got", n, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Reset relative to start.
+		nn, err := map0.Seek(0, 0)
+		if nn != 0 || err != nil {
+			t.Error("seeking: want 0 <nil>, got", nn, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Write to, the full buffer.
+		var buf bytes.Buffer
+		nn, err = map0.WriteTo(&buf)
+		if nn != int64(size) || err != nil {
+			t.Error("writing to: want", size, "<nil>, got", nn, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Write to, but buffers already consumed.
+		nn, err = map0.WriteTo(&buf)
+		if nn != 0 || err != nil {
+			t.Error("writing to: want 0 <nil>, got", nn, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Read at everything.
+		n, err = map0.ReadAt(everything, 0)
+		if n != size || err != nil {
+			t.Error("read at 0 everything: want", size, "<nil>, got", n, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Read at running past end of buffer.
+		n, err = map0.ReadAt(everything, 1)
+		if n != size-1 || err != io.EOF {
+			t.Error("read at 0 everything: want", size-1, "<nil>, got", n, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Read at starting past the end of the buffer.
+		n, err = map0.ReadAt(everything, int64(size))
+		if n != 0 || err != io.EOF {
+			t.Error("read at 0 everything: want 0 io.EOF, got", n, err)
+			releaseAll(toRelease, t)
+			continue
+		}
+
+		// Read at starting at a negative value.
+		n, err = map0.ReadAt(everything, -1)
+		if n != 0 || err == nil {
+			t.Error("read at 0 everything: want 0 cl: MappedBuffer.ReadAt: negative offset, got", n, err)
 			releaseAll(toRelease, t)
 			continue
 		}
